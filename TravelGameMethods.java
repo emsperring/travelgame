6b5944aecec4d9d0c081800127aefbca74feaf45
emsperring <124449255+emsperring@users.noreply.github.com>
2023-02-05 00:01:21 -0500

    Update TravelGameMethods.java

--- TravelGameMethods.java
+++ TravelGameMethods.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class TravelGameMethods {
 	private static Scanner keyboard = new Scanner(System.in);
 	private static int score = 0;
-	private static Fact[] factList = { 
+    private static boolean exited = false;
+	private static Fact[] factList = {
 			new Fact("Italy has a free public wine fountain", "T"),
 			new Fact("China is the largest country landwise", "F"),
 			new Fact("China has the largest population", "T"),
@@ -24,20 +25,77 @@ public class TravelGameMethods {
 		};
 
 	public static void main(String[] args) {
-		start();
+        System.out.println("Welcome!");
+        choice();
+
 	}
 
-	public static void start() {
+    private static void choice() {
+        exited = false;
+        System.out.println("Type 0 to quit, 1 to play RANDOM FACTS, or 2 for CITY LIFE LEVELS.");
+        int choice = keyboard.nextInt();
+        String answer = keyboard.nextLine();
+        boolean decided = false;
+        while (!decided) {
+            if (choice == 1) {
+                decided = true;
+                startFacts();
+            } else if (choice == 2) {
+                decided = true;
+                startCity();
+            } else if (choice == 0) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            } else {
+                System.out.println("invalid option: select 1 or 2");
+            } //if
+        } //while
+    } //choice
+
+    public static void startCity() {
+        System.out.println("Welcome to CITY LIFE LEVELS!");
+        System.out.println("This is a text based game.");
+        System.out.println("First, enter the name of a city, then guess its average quality of life");
+        System.out.println("If you are within 10, you earn 10 points. If you are within 5, you earn 20 points");
+
+        boolean cityFound = false;
+        while (!cityFound) {
+            System.out.println("Enter a city or exit (e):");
+            String city = keyboard.nextLine();
+            if (LocationQuiz.request(city) >= 0) {
+                cityFound = true;
+                cityGuess(LocationQuiz.request(city));
+            } else {
+                System.out.println("invalid city name: " + city);
+            } //if
+        } //while
+    } //startCity
+
+    public static void cityGuess(int value) {
+        System.out.println("Enter your predicted city score:");
+        int guess = keyboard.nextInt();
+        System.out.println("Correct answer: " + value);
+        if (Math.abs(guess - value) <= 5) {
+            score += 20;
+        } else if (Math.abs(guess - value) <= 10) {
+            score += 10;
+        }
+        System.out.println("Your score is now " + score);
+        choice();
+    } //cityGuess
+
+
+	public static void startFacts() {
 			System.out.println("Welcome to RANDOM FACTS! \nThis is a text based game where we tell you a fact and answer T for true or F for false."
 					+"\nType E to Exit");
 			play();
 	}
-	
+
 	private static void question(String userAnswer, String answer) {
 		userAnswer = userAnswer.trim();
 		if (userAnswer.equalsIgnoreCase("e")) {
-			System.out.println("Goodbye.");
-			System.exit(1);
+            exited = true;
+            choice();
 		} else if (userAnswer.equalsIgnoreCase(answer)) {
 			score += 10;
 			System.out.println("That is correct! Your score is now " + score);
@@ -45,14 +103,16 @@ public class TravelGameMethods {
 			System.out.println("That is WRONG. NEXT QUESTION!");
 		}
 	}
-		
+
 	private static void play() {
 		for(int i = 0; i < factList.length; i++) {
-			System.out.println(factList[i].fact);
-			String userAnswer = keyboard.nextLine();
-			question(userAnswer, factList[i].answer);
+            if (!exited) {
+                System.out.println(factList[i].fact);
+                String userAnswer = keyboard.nextLine();
+                question(userAnswer, factList[i].answer);
+            } //if
 		}
 		System.out.print("Thank you for playing!");
 	}
-	
+
 }
